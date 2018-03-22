da00e6245747b26fa8010ebdd633a642badcdcef
Jonanthan Bélanger <joe_belanger@hotmail.com>
2018-03-21 22:27:22 -0400

    typo

--- SportsInformationsManager/Program.cs
+++ SportsInformationsManager/Program.cs
@@ -13,37 +13,7 @@ namespace SportsInformationsManager
     {
 		static void Main(string[] args)
 		{
-			using (var context = new Context())
-			{
-
-				var competitions = context.Competitions.ToList();
-
-				foreach (var c in competitions)
-				{
-
-
-					Console.WriteLine("Emplacement : {0}", c.Location);
-					Console.WriteLine("Date : {0}", c.CompetitionDate);
-					Console.WriteLine("Compétition de {0}", context.Sports.FirstOrDefault(s => s.Id == c.SportId).Name);
-					Console.WriteLine();
-					Console.WriteLine("Athlètes :");
-
-
-					var ac = c.Athletes.ToList();
-
-					foreach(var a in ac)
-					{
-						Console.WriteLine("Nom = {0} {1}",context.Athletes.FirstOrDefault(at => at.Id == a.AthleteId).FirstName, context.Athletes.FirstOrDefault(at => at.Id == a.AthleteId).LastName);
-						Console.WriteLine();
-					}
-
-
-				}
-
-				
-
-				Console.ReadLine();
-			}
+			
 		}
     }
 }
